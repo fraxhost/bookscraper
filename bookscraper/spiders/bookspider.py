2e093adfb74d565fd2fed8abb8dd534eb99420fb
Ahmed Ryan <ahmedryanfaiyaz@gmail.com>
2025-02-25 12:35:46 +0600

    feature: fake user agents and fake browser headers implemented;

--- bookscraper/spiders/bookspider.py
+++ bookscraper/spiders/bookspider.py
@@ -1,53 +1,70 @@
+"""BookSpider for scraping book details from a website using Scrapy."""
+
+import random
 import scrapy
 from bookscraper.items import BookItem
 
+
 class BookspiderSpider(scrapy.Spider):
+    """Spider for scraping book details from a website using Scrapy."""
+
     name = "bookspider"
     allowed_domains = ["books.toscrape.com"]
     start_urls = ["https://books.toscrape.com"]
 
+    custom_settings = {
+        "FEEDS": {
+            "booksdata.json": {"format": "json", "overwrite": True},
+        }
+    }
+
     def parse(self, response):
-        books = response.css('article.product_pod')
+        """Parses the book listing page and follows pagination & book links."""
+
+        books = response.css("article.product_pod")
 
         for book in books:
-            next_page = response.css('li.next a::attr(href)').get()
+
+            relative_url = book.css("h3 a::attr(href)").get()
+            if "catalogue/" in relative_url:
+                relative_url = "https://books.toscrape.com/" + relative_url
+            else:
+                relative_url = "https://books.toscrape.com/catalogue/" + relative_url
+
+            yield response.follow(relative_url, callback=self.parse_book_page)
+
+        next_page = response.css("li.next a::attr(href)").get()
 
         if next_page is not None:
-            relative_url = response.css('h3 a::attr(href)').get()
-            
-            if 'catalogue/' in relative_url:
-                relative_url = 'https://books.toscrape.com/' + relative_url
+            if "catalogue/" in next_page:
+                next_page_url = "https://books.toscrape.com/" + next_page
             else:
-                relative_url = 'https://books.toscrape.com/catalogue/' + relative_url
+                next_page_url = "https://books.toscrape.com/catalogue/" + next_page
 
-            yield response.follow(relative_url, callback = self.parse_book_page)
-
-        next_page = response.css('li.next a::attr(href)').get()
-
-        if next_page is not None:
-            if 'catalogue/' in next_page:
-                next_page_url = 'https://books.toscrape.com/' + next_page
-            else:
-                next_page_url = 'https://books.toscrape.com/catalogue/' + next_page
-
-            yield response.follow(next_page_url, callback = self.parse)
+            yield response.follow(next_page_url, callback=self.parse)
 
     def parse_book_page(self, response):
-        table_rows = response.css('table tr')
+        """Extracts book details from the book's individual page."""
+
+        table_rows = response.css("table tr")
         book_item = BookItem()
-         
-        book_item['url'] = response.url,
-        book_item['title'] = response.css('.product_main h1::text').get(),
-        book_item['upc'] = table_rows[0].css('td::text').get(),
-        book_item['product_type'] = table_rows[1].css('td::text').get(),
-        book_item['price_excl_tax'] = table_rows[2].css('td::text').get(),
-        book_item['price_incl_tax'] = table_rows[3].css('td::text').get(),
-        book_item['tax'] = table_rows[4].css('td::text').get(),
-        book_item['availability'] = table_rows[5].css('td::text').get(),
-        book_item['num_reviews'] = table_rows[6].css('td::text').get(),
-        book_item['stars'] = response.css('p.star-rating').attrib['class'],
-        book_item['category'] = response.xpath("//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()").get(),
-        book_item['description'] = "response.xpath('do it yourself')",
-        book_item['price'] = response.css('p.price_color::text').get(),
+
+        book_item["url"] = (response.url,)
+        book_item["title"] = (response.css(".product_main h1::text").get(),)
+        book_item["upc"] = (table_rows[0].css("td::text").get(),)
+        book_item["product_type"] = (table_rows[1].css("td::text").get(),)
+        book_item["price_excl_tax"] = (table_rows[2].css("td::text").get(),)
+        book_item["price_incl_tax"] = (table_rows[3].css("td::text").get(),)
+        book_item["tax"] = (table_rows[4].css("td::text").get(),)
+        book_item["availability"] = (table_rows[5].css("td::text").get(),)
+        book_item["num_reviews"] = (table_rows[6].css("td::text").get(),)
+        book_item["stars"] = (response.css("p.star-rating").attrib["class"],)
+        book_item["category"] = (
+            response.xpath(
+                "//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()"
+            ).get(),
+        )
+        book_item["description"] = ("response.xpath('do it yourself')",)
+        book_item["price"] = (response.css("p.price_color::text").get(),)
 
         yield book_item
